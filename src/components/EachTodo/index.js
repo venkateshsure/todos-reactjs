ed4ad54c93160994e01477c8a1617eebca6595de
venkateshsure <venky007sure@gmail.com>
2024-02-29 11:30:05 +0530

    need to make values to priority and status from user

--- src/components/EachTodo/index.js
+++ src/components/EachTodo/index.js
@@ -1,11 +1,23 @@
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { MdOutlineTipsAndUpdates } from "react-icons/md";
+
+import './index.css'
 
 const EachTodo=(props)=>{
-   const {eachTodo}=props
-   console.log(eachTodo)
+   const {eachTodo,onUpdateTodo,onDeleteTodo}=props
    const {id,todo,status,priority}=eachTodo
     return (
-        <li>
-            <p>{todo}</p>
+        <li className="todo-item-container">
+            <input type="checkbox" id={id} className='checkbox-input'/>
+            <div className="label-container">
+                <label  htmlFor={id} className='checkbox-label'>{todo}</label>
+                <div onClick={()=>onDeleteTodo(id)} className="delete-icon-container">
+                    <RiDeleteBin6Line className="delete-icon"/>
+                </div>
+                <div onClick={()=>onUpdateTodo(id)} className="delete-icon-container">
+                    <MdOutlineTipsAndUpdates className="delete-icon"/>
+                </div>
+            </div>
         </li>
     )
 }
--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -1,3 +1,4 @@
+
 import {useState,useEffect} from 'react';
 
 
@@ -5,7 +6,12 @@ import EachTodo from '../EachTodo'
 import './index.css'
 
 const Todos=()=>{
+    const [todoText,setTodoText]=useState("")
     const [todos,setTodos]=useState([])
+
+    const onEnterTodo=(event)=>{
+        setTodoText(event.target.value)
+    }
 
     useEffect(()=>{
         const fetchTodos= async ()=>{
@@ -18,40 +24,101 @@ const Todos=()=>{
         };
         const response= await fetch(url,options)
         const data= await response.json()
-       // console.log(data)
         setTodos(data)
 
         }
         fetchTodos()
     },[])
 
-    console.log(todos)
+    const addNewTodo= async  ()=>{
+        const todoLength=todos.length
+        console.log(todoText)
+        let newTodo = {
+            "id": parseInt(todoLength)+1,
+            "priority": "HIGH",
+            "status": "IN PROGRESS",
+            "todo": todoText,
+        };
+        let url="https://backenedtodo-app-production.up.railway.app/todos/"
+        let options = {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+                "Accept": 'application/json',
+            },
+            body:JSON.stringify(newTodo)
+        };
+        const response= await fetch(url,options)
+        console.log(response)
+        const data= await response.text()
+        setTodoText("")
+        setTodos([...todos,newTodo]);
+        console.log(data)
+    }
+
+
+    const onUpdateTodo=async (id)=>{
+        const url = `https://backenedtodo-app-production.up.railway.app/todos/${id}/`;
+
+        let updatedData = {
+            "todo":todoText
+        };
+        
+        let options = {
+            method: "PUT",
+            headers: {
+                "Content-Type": 'application/json',
+                "Accept": 'application/json',
+            },
+            body: JSON.stringify(updatedData),
+        };
+        const response= await fetch(url,options)
+        const responseText=await response.text()
+        const updatedTodos = todos.map(todo => todo.id === id ? { ...todo, todo: todoText } : todo);
+        setTodos(updatedTodos);
+        console.log(responseText) 
+        setTodoText("")
+    }
+
+    const onDeleteTodo= async   (todoId)=>{
+        console.log(todoId)
+        let deleteUrl = `https://backenedtodo-app-production.up.railway.app/todos/${todoId}/`;
+
+        let deleteOptions = {
+            method: 'DELETE',
+        };
+
+       const response=await  fetch(deleteUrl, deleteOptions)
+        const responseText=await response.text()
+        const todosAfterDelete=todos.filter(eachTodo=>(eachTodo.id!==todoId))
+        setTodos(todosAfterDelete)
+        console.log(responseText)
+    }
+
     return (
         <div className="todos-bg-container">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
+                <div className="con">
                     <h1 className="todos-heading">Todos</h1>
                     <h1 className="create-task-heading">
                         Create <span className="create-task-heading-subpart">Task</span>
                     </h1>
-                    <input type="text" id="todoUserInput" className="todo-user-input" placeholder="What needs to be done?" />
-                    <button className="button" id="addTodoButton">Add</button>
+                    <input value={todoText} type="text" className="todo-user-input" placeholder="What needs to be done?" onChange={onEnterTodo}/>
+                    <div>
+                        <button className="button" onClick={addNewTodo}>Add</button>
+                    </div>
                     <h1 className="todo-items-heading">
                         My <span className="todo-items-heading-subpart">Tasks</span>
                     </h1>
-                    <ul className="todo-items-container" id="todoItemsContainer">
+                    <ul className="todo-items-container">
                         {
                             todos.map((eachTodo)=>{
-                                return <EachTodo eachTodo={eachTodo} key={eachTodo.id}/>
+                                return <EachTodo eachTodo={eachTodo} key={eachTodo.id} onUpdateTodo={onUpdateTodo} onDeleteTodo={onDeleteTodo}/>
                             })
                         }
                     </ul>
                     
                 </div>
             </div>
-        </div>
-    </div>
     )
 }
 
